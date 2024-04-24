96ce0a6bf89ba162e54293e639977cd2a0a6495d
Chris Diggle <chrisdiggle8@outlook.com>
2024-04-24 18:49:46 +0000

    set up axios interceptors to refresh tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -5,3 +5,8 @@ import axios from "axios";
 axios.defaults.baseURL = "https://walkthrough-drf-api-1c81384c884e.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+// Below intercepts the request
+export const axiosReq = axios.create();
+// Below intercepts the response
+export const axiosRes = axios.create();
