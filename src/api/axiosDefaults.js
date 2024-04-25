63775e77ea16ca488879b5a0af0276540ec99abc
Chris Diggle <chrisdiggle8@outlook.com>
2024-04-25 17:32:52 +0000

    create a working form so users can create posts

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,12 +1,8 @@
-// The code in this file was copied from the 'Moments' walkthrough project.
-
 import axios from "axios";
 
 axios.defaults.baseURL = "https://walkthrough-drf-api-1c81384c884e.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
-// Below intercepts the request
 export const axiosReq = axios.create();
-// Below intercepts the response
 export const axiosRes = axios.create();
