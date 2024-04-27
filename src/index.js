1780b19920f0cba3dfa03b6436fa5a889cb0cd82
Chris Diggle <chrisdiggle8@outlook.com>
2024-04-27 09:55:54 +0000

    create the Context.Provider for our profileData

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 import { CurrentUserProvider } from './contexts/CurrentUserContext';
+import { ProfileDataProvider } from './contexts/ProfileDataContext';
 
 ReactDOM.render(
     <Router>
       <CurrentUserProvider>
-      <App />
+        <ProfileDataProvider>
+          <App />
+        </ProfileDataProvider>
       </CurrentUserProvider>
     </Router>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
